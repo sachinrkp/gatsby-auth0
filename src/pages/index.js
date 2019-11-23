eea97dd24f9a8c5b1cbcdfab6fec92b2e6670b9d
Sachin Kumar Patel <sachinkumarpatel1996@gmail.com>
2019-11-24 03:34:19 +0530

    final commit

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,3 +1,10 @@
+// ./src/pages/index.js
 import React from "react"
+import { Link } from "gatsby"
 
-export default () => <div>Hello world!</div>
+export default () => (
+  <div>
+    <p>Hello Gatsby!</p>
+    <Link to="/account">Go to your account</Link>
+  </div>
+)
